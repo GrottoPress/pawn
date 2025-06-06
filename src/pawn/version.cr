77c44783b2d478d6658e40be3d374651f4db63b6
akadusei <attakusiadusei@gmail.com>
2025-06-06 13:17:31 +0000

    Fix compile error calling `.compare_versions` with `Pawn::VERSION`
    ```
    Error: expected first argument to 'compare_versions' to be a StringLiteral, SymbolLiteral or MacroId, not MacroExpression
    ```

--- src/pawn/version.cr
+++ src/pawn/version.cr
@@ -1,3 +1,7 @@
 struct Pawn
-  VERSION = {{ `shards version "#{__DIR__}"`.chomp.stringify }}
+  private macro set_version
+    VERSION = {{ `shards version "#{__DIR__}"`.chomp.stringify }}
+  end
+
+  set_version
 end
